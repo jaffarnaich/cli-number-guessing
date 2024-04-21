65d86df027213e14077f254cb66f7658380ca2ea
jaffarnaich <naaich007@gmail.com>
2024-04-21 16:44:29 +0500

    number guessing game

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// 1) computer generate a random number - done
+
+// 2) user inpur for gussing number - done
+
+// 3) compair user input with computer generated number and show result
+
+const ramdomNumber = Math.floor(Math.random() * 6 +1);
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "please guess a number between 1-6:",
+  },
+]);
+
+if (answers.userGuessedNumber === ramdomNumber ){
+    console.log("Congratulations! you guessed right number.")
+}else {
+    console.log ("You guessed wrong number")
+}
